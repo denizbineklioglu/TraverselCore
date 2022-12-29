8241be839b49cdbaeab58956b8f5dca34f6cb7ba
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2022-12-29 23:34:56 +0300

    29.12

--- TraversalCoreProje/Startup.cs
+++ TraversalCoreProje/Startup.cs
@@ -12,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TraversalCoreProje.Models;
 
 namespace TraversalCoreProje
 {
@@ -28,11 +29,12 @@ namespace TraversalCoreProje
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<Context>();
-            services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<Context>();
+            services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<Context>()
+            .AddErrorDescriber<CustomIdentityValidator>().AddEntityFrameworkStores<Context>();
             services.AddControllersWithViews();
 
-            // Proje seviyesinde Authorization
 
+            // Proje seviyesinde Authorization
             services.AddMvc(config =>
             {
                 var policy = new AuthorizationPolicyBuilder()
@@ -70,6 +72,15 @@ namespace TraversalCoreProje
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+
         }
     }
 }
