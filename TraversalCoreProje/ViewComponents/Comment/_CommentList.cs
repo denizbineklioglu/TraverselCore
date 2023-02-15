30911c2cd6e52caa284f08213468e3391f0afc73
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2023-02-15 17:20:09 +0300

    comment user

--- TraversalCoreProje/ViewComponents/Comment/_CommentList.cs
+++ TraversalCoreProje/ViewComponents/Comment/_CommentList.cs
@@ -1,15 +1,27 @@
-﻿using BusinessLayer.Concrete;
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace TraversalCoreProje.ViewComponents.Comment
 {
     public class _CommentList:ViewComponent
     {
-        CommentManager cm = new CommentManager(new EfCommentDal());
+        private readonly ICommentService _commentService;
+        private readonly Context _context;
+        public _CommentList(ICommentService commentService, Context context)
+        {
+            _commentService = commentService;
+            _context = context;
+        }
+
         public IViewComponentResult Invoke(int id)
         {
-            var values = cm.TGetDestinationById(id); 
+            ViewBag.commentCount = _context.Comments.Where(x => x.DestinationID == id).Count();
+            var values = _commentService.TGetListCommentWithDestinationAndUser(id); 
             return View(values);
         }
     }
