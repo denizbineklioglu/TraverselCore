20e8b9737e14eeb72e620884912aada40da3193a
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2023-02-17 17:04:27 +0300

    dashboardda son düzenlemeler

---
+++ TraversalCoreProje/Controllers/ContactUsController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace TraversalCoreProje.Controllers
+{
+    [AllowAnonymous]
+    public class ContactUsController : Controller
+    {
+        private readonly IContactUsService _contactService;
+        public ContactUsController(IContactUsService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _contactService.TGetList();
+            return View(values);
+        }
+    }
+}
