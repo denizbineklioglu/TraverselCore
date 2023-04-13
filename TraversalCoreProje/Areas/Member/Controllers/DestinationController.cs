36d0c673a1e41f5a1d30b516883e2afe855f3c57
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2023-04-13 16:28:46 +0300

    destination search

--- TraversalCoreProje/Areas/Member/Controllers/DestinationController.cs
+++ TraversalCoreProje/Areas/Member/Controllers/DestinationController.cs
@@ -2,11 +2,12 @@
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace TraversalCoreProje.Areas.Member.Controllers
 {
     [Area("Member")]
-    [AllowAnonymous]
+    [Route("Member/[controller]/[action]")]
     public class DestinationController : Controller
     {
         DestinationManager dm = new DestinationManager(new EfDestinationDal());
@@ -14,6 +15,17 @@ namespace TraversalCoreProje.Areas.Member.Controllers
         {
             var values = dm.TGetList();
             return View(values);
+        }
+
+        public IActionResult GetCitiesSearchByName(string searchString)
+        {
+            ViewData["CurrentFilter"] = searchString;
+            var values = from x in dm.TGetList() select x;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                values = values.Where(y => y.City.Contains(searchString));
+            }
+            return View(values.ToList());
         }
     }
 }
